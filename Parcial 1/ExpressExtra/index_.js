efd186f4cd4cbb92deb8b4dd4e40cce88e6f173a
ErikaBernabe <erikayazmin.27@gmail.com>
2024-09-03 22:21:22 -0600

    03_09_2024

--- Parcial 1/ExpressExtra/index_.js
+++ Parcial 1/ExpressExtra/index_.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors')
-
-// app.use(cors())
-
+ 
+// const  cors  = require ('cors');
+// app.use(cors());
+ 
+ 
 app.get('/',(req,res)=>{
     res.send('Server Express contestando a peticion get')
 })
-
+ 
 app.post('/',(req,res)=>{
     res.send('Server Express contestando a peticion post')
 })
-
-app.listen(3000,()=>{
-    console.log('Server Express escuchando en el puerto 3000')
+ 
+app.listen(3001,()=>{
+    console.log('http://localhost:3000')
 })
