a3230afd295fd0667b2034879ba1c896639aa037
ErikaBernabe <erikayazmin.27@gmail.com>
2024-11-07 10:38:17 -0600

    actualizacion

---
+++ Parcial 3/TestNodeChai/test/areas.test.js
@@ -0,0 +1,9 @@
+import assert from 'node:assert';
+import test from 'node:test';
+import * as areas from "../src/areas.js"
+// import { assert } from 'node:console';
+
+test ("Si le mando un 2 debe dar 4",()=>{
+    let res = areas.areaCuadrado(2);
+    assert.strictEqual(res,4);
+})
